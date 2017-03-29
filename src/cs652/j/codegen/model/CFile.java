94e532484ceb54ec0b584ef5dc11553923d04378
Anjani Bajaj <aabajaj2@dons.usfca.edu>
2017-03-29 16:31:43 -0700

    Travis check commit 2

--- src/cs652/j/codegen/model/CFile.java
+++ src/cs652/j/codegen/model/CFile.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 public class CFile extends OutputModelObject {
 	public final String fileName;
-	@ModelElement public List<ClassDef> classes = new ArrayList<>();
-	@ModelElement public MainMethod main;
+//	@ModelElement public List<ClassDef> classes = new ArrayList<>();
+//	@ModelElement public MainMethod main;
 
 	public CFile(String fileName) {
 		this.fileName = fileName;
